2e3eccfeea1832aa2087921fd2bac55b112b5ff2
wochowskaj83 <wochowskaj@gmail.com>
2023-04-13 16:25:16 +0200

    z poprawka

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let num1 = 2;
 let num2 = 5;
 
-if (num1 > num2) {
-    console.log(num1)
+function greater(a, b) {
+if (a > b) {
+    return a;
 } else {
-    console.log(num2)
-}
+    return b;
+}}
 
+console.log(greater(num1,num2))
